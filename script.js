19f4ff40677bb33a7443d715990f42c65fdfba5e
marista93 <maria.st.bel@gmail.com>
2026-04-06 22:42:12 +0300

    ok

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+const wavelengthSlider = document.getElementById("wavelength");
+const wavelengthValue = document.getElementById("wavelengthValue");
 
+function updateWavelengthValue() {
+  wavelengthValue.value = `${wavelengthSlider.value} nm`;
+}
+
+wavelengthSlider.addEventListener("input", updateWavelengthValue);
+updateWavelengthValue();
